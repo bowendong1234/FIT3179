c16cef3072e67fbab42028dbda5a49f90b2e1d03
bowendong1234 <bdon0009@student.monash.edu>
2024-10-06 21:38:07 +1100

    Formatting fixes and changing rendering to svg to stop it from looking so blurry

--- bushfire_vis.js
+++ bushfire_vis.js
@@ -1,11 +1,11 @@
 var vg_1 = "bushfire_vis.vg.json";
-vegaEmbed("#bushfire_chart", vg_1).then(function(result) {
+vegaEmbed("#bushfire_chart", vg_1, {renderer: 'svg'}).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "inhalants_and_adrenergics.vg.json";
-vegaEmbed("#inhalants_and_adrenergics_chart", vg_2).then(function(result) {
+vegaEmbed("#inhalants_and_adrenergics_chart", vg_2, {renderer: 'svg'}).then(function(result) {
 }).catch(console.error);
 
 var vg_3 = "inhaler_pres_shortness.vg.json";
-vegaEmbed("#inhaler_sales", vg_3).then(function(result) {
+vegaEmbed("#inhaler_sales", vg_3, {renderer: 'svg'}).then(function(result) {
 }).catch(console.error);
